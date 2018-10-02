99e140d265f5eae1a745569513a901406f7ab364
Koishi Masato <masato.koishi@bizreach.co.jp>
2018-10-02 09:07:02 +0900

    create autoscaling group in terraform

---
+++ autoscale-example/asg.tf
@@ -0,0 +1,19 @@
+resource "aws_autoscaling_group" "rails5-sample-autoscale-group" {
+    desired_capacity          = 1
+    health_check_grace_period = 60
+    health_check_type         = "ELB"
+    launch_configuration      = "rails5-sample-autoscale-2"
+    max_size                  = 3
+    min_size                  = 1
+    name                      = "rails5-sample-autoscale-group"
+    vpc_zone_identifier       = ["subnet-05a3eef07771a5a4e", "subnet-0be4902b9f2101452"]
+    target_group_arns = ["${aws_lb_target_group.rails5-sample-listener.arn}"]
+
+    tag {
+        key   = "Name"
+        value = "rails-sample-auto-scale"
+        propagate_at_launch = true
+    }
+
+}
+
