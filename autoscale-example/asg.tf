437d11ab7c32c499863e9126e51730374e840cb0
Koishi Masato <masato.koishi@bizreach.co.jp>
2018-10-05 08:19:25 +0900

    create gatling rails simulation

--- autoscale-example/asg.tf
+++ autoscale-example/asg.tf
@@ -8,6 +8,7 @@ resource "aws_autoscaling_group" "rails5-sample-autoscale-group" {
     name                      = "rails5-sample-autoscale-group"
     vpc_zone_identifier       = ["subnet-05a3eef07771a5a4e", "subnet-0be4902b9f2101452"]
     target_group_arns = ["${aws_lb_target_group.rails5-sample-listener.arn}"]
+    force_delete              = true
 
     tag {
         key   = "Name"
@@ -15,5 +16,19 @@ resource "aws_autoscaling_group" "rails5-sample-autoscale-group" {
         propagate_at_launch = true
     }
 
+}
+
+resource "aws_autoscaling_policy" "rails5-sample-asg-cpu-policy" {
+  name                   = "rails5-sample-asg-cpu-policy"
+  adjustment_type        = "PercentChangeInCapacity"
+  policy_type            = "TargetTrackingScaling"
+  autoscaling_group_name = "${aws_autoscaling_group.rails5-sample-autoscale-group.name}"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 40.0
+  }
 }
 
